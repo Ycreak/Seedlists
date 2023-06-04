f7f1345b6f53406a09acd0b25987354fb0209f49
Ycreak <y.creak@hotmail.com>
2023-06-04 22:58:12 +0200

    Removed unused files

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\IntroductionController;
 use App\Http\Controllers\SeedlistsController;
 use App\Http\Controllers\PlantsController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,34 +20,17 @@ Route::get('/', function () {
     return view('introduction.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::get('/show_seedlist/{id}', [SeedlistsController::class, 'show_seedlist']);
 Route::get('/show_plant/{id}', [PlantsController::class, 'show_plant']);
 
-
 Route::resource('introduction', IntroductionController::class)
 ->only(['index', 'store']);
-// ->middleware(['auth', 'verified']);
 
 Route::resource('plants', PlantsController::class)
 ->only(['index', 'store', 'add']);
-// ->middleware(['auth', 'verified']);
 
 Route::post('/add_plant', [PlantsController::class, 'add'])
 ->name('plants.add');;
 
 Route::resource('seedlists', SeedlistsController::class)
     ->only(['index', 'store']);
-    // ->middleware(['auth', 'verified']);
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-
-require __DIR__.'/auth.php';
